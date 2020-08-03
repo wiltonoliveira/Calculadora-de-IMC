ba3039fcb2d763dd7acb3f0c83695f05f34cf322
Wilton Oliveira <oliveirajrwilton@gmail.com>
2020-08-02 22:56:07 -0300

    Inserindo Status

--- scripts.js
+++ scripts.js
@@ -8,9 +8,22 @@ function calcular(){
     let idade = Number(formIdade.value);
 
     let saida = document.getElementById('resultado');
+    let status;
     
     let imc = peso / (altura**2);
 
+    if(imc <= 24.9){
+        status = "Peso Ideal";
+    } else if(imc < 29.9){
+        status = "Sobrepeso";
+    } else if(imc < 34.9){
+        status = "Obesidade Grau I";
+    } else if(imc < 39.9){
+        status = "Obesidade Grau II";
+    } else {
+        status = "Obesidade Grau III";
+    }
+
     saida.style.textAlign = 'center'
-    saida.innerHTML = `Seu IMC é de ${imc.toFixed(2)}!<br>`
+    saida.innerHTML = `Seu IMC é de ${imc.toFixed(2)}!<br> Você está com ${status};
 }
